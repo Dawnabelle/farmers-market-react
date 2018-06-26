3500f21552a56d65d7a597f99d538813a5310809
Dawn Mott and Kristen Chellis <kristen@>
2018-06-26 10:50:26 -0700

    add content for Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header(){
+  return (
+    <div>
+      <h1>Welcome to Avery Organics!</h1>
+      <h2>See below for our weekly farmer's market schedule and monthly produce selection</h2>
+    </div>
+  )
+}
