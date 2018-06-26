0c239199dc208a9353a02c38cae4cbcf44cdef14
Dawn Mott and Kristen Chellis <kristen@>
2018-06-26 10:33:52 -0700

    add Day component

---
+++ src/components/Day.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Day(props){
+  return(
+    <div>
+      <h3>{props.weekday}</h3>
+      <h5>{props.location} - {props.booth}</h5>
+      <h5>props.hours</h5>
+    </div>
+  );
+}
+
+Day.propTypes = {
+  weekday: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  hours: PropTypes.string.isRequired,
+  booth: PropTypes.string
+};
+
+export default Day;
